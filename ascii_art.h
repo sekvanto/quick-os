c95cf19d649bab842ba7c485b27823a02f7876d5
sekvanto <zip.aioniplaitnereffid@gmail.com>
2022-12-03 02:34:44 +0800

    render colorful ascii logo

---
+++ ascii_art.h
@@ -0,0 +1,9 @@
+#ifndef ASCII_ART_H
+#define ASCII_ART_H
+
+#include "common.h"
+#include "vga_terminal.h"
+
+void render_logo();
+
+#endif
--- kernel.c
+++ kernel.c
@@ -2,6 +2,7 @@
 
 #include "libc.h"
 #include "vga_terminal.h"
+#include "ascii_art.h"
 
 /* Check if the compiler thinks you are targeting the wrong operating system. */
 #if defined(__linux__)
@@ -20,13 +21,7 @@ void kernel_main(void)
 	terminal_setcolor(VGA_COLOR_LIGHT_BLUE);
 
 	/* Newline support is left as an exercise. */
-	for (size_t i = 0; i < VGA_HEIGHT / 3; i++) {
-		terminal_writestring("Hello, kernel World!\nI'm at new line\nOne extra line\n");
-	}
-	terminal_writestring("An additional line\nSecond line\n");
-	terminal_putentryat('1', VGA_COLOR_RED, 20, VGA_HEIGHT - 1);
-	terminal_putentryat('2', VGA_COLOR_LIGHT_RED, 50, VGA_HEIGHT - 1);
-	terminal_writestring("\nAnd one more string\n");
-	terminal_setcolor(VGA_COLOR_CYAN);
-	terminal_writestring("\n\nTwo empty lines in between");
+	terminal_writestring("Hello, kernel World!\nOne extra line\n\n");
+
+	render_logo();
 }
