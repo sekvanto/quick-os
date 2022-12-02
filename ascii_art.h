920649604a0404198c9feaea92c46f5fc5537c7b
sekvanto <zip.aioniplaitnereffid@gmail.com>
2022-12-03 04:01:12 +0800

    replace contents with code from meaty skeleton osdev wiki tutorial

--- ascii_art.h
+++
@@ -1,9 +0,0 @@
-#ifndef ASCII_ART_H
-#define ASCII_ART_H
-
-#include "common.h"
-#include "vga_terminal.h"
-
-void render_logo();
-
-#endif
---
+++ iso.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+. ./build.sh
+
+mkdir -p isodir
+mkdir -p isodir/boot
+mkdir -p isodir/boot/grub
+
+cp sysroot/boot/quick-os.kernel isodir/boot/quick-os.kernel
+cat > isodir/boot/grub/grub.cfg << EOF
+menuentry "quick-os" {
+	multiboot /boot/quick-os.kernel
+}
+EOF
+grub-mkrescue -o quick-os.iso isodir
